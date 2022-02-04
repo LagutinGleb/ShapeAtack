bddb6efb28645b832c531ccc261176ea89dab69a
LagutinGleb <96201919+LagutinGleb@users.noreply.github.com>
2022-02-04 17:19:42 +0300

    Beta

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,19 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
     public GameObject canvasGame;
     public GameObject canvasStart;
     public GameObject fire;
+    public Image[] enemyImages = new Image[3];
 
     public void ButtonStart()
     {
         canvasStart.gameObject.SetActive(false);
         Time.timeScale = 1;
         fire.GetComponent<Fire>().enabled = true;
+
+        for (int i = 0; i < enemyImages.Length; i++)
+        {
+            enemyImages[i].enabled = false;
+        }
     }
 
     public void ButtonRestart()
